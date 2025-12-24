4c39f882cfd8753f1d80b734c6289ee78cd9db10
keramicheskiy <121174007+keramicheskiy@users.noreply.github.com>
2025-12-24 15:27:50 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keramicheskiy/development/University/oop/lab_5
-# Build directory: /home/keramicheskiy/development/University/oop/lab_5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/keramicheskiy/development/University/oop/lab_5/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/keramicheskiy/development/University/oop/lab_5/build/tests[1]_tests.cmake")
-  include("/home/keramicheskiy/development/University/oop/lab_5/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
